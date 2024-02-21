ffb217d34d2a5c0911ed3567d7bcc972dd9dd8ee
Mello Birkan <mgbirkan@icloud.com>
2024-02-21 02:03:48 -0300

    Initial Commit

---
+++ iSky/iSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSkyApp.swift
+//  iSky
+//
+//  Created by Marcello Gonzatto Birkan on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iSkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
